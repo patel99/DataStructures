ee9b9ed4ffec89c11b43cca95b993458f4062550
pratik <pratik.patel.sh@hotmail.com>
2023-06-22 20:51:05 -0700

    Remove SourceTree

--- src/Heap/Test.java
+++ src/Heap/Test.java
@@ -1,15 +1,10 @@
 package Heap;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.util.Scanner;
-
 /**
  * Created by Pratik on 2/21/2016.
  */
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         MaxHeap max = new MaxHeap(25);
         MinHeap min = new MinHeap(25);
         for(int i=0; i<25; i++){
